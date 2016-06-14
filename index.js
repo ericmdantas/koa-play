efd15fa30ca8bd70b8a4899aaac2b42c37908c73
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-06-13 22:06:56 -0300

    .

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+const https = require('https');
+const fs = require('fs');
 const Koa = require('koa');
 const router = require('koa-router')();
+const serve = require('koa-static');
 const app = new Koa();
 
 const PORT = 3000;
 
 app.use(router.routes())
+app.use(serve('client/dev'))
 
-router.get('/', function *() {
-    this.body = "wtf";
+router
+  .get('/wtf', function *() {
+    this.body = {"wtf": true};
   });
 
-app.listen(PORT);
+const opts = {
+  key: fs.readFileSync(__dirname + '/cert/server.key'),
+  cert: fs.readFileSync(__dirname + '/cert/server.crt')
+}
+
+https.createServer(opts, app.callback()).listen(PORT);
 
 console.log(`running: ${PORT}`);
