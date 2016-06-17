975772de15c5e173fee31b14334f88a672ddde1c
Eric Mendes Dantas <ericdantas0@gmail.com>
2016-06-16 21:33:59 -0300

    .

--- server/index.js
+++ server/index.js
@@ -7,13 +7,17 @@ const app = new Koa();
 
 const PORT = 3000;
 
+class Ctrl {
+  *go() {
+    this.body = {wtf: true};
+  }
+}
+
 app.use(router.routes())
 app.use(serve(process.cwd() + '/client/dev'))
 
 router
-  .get('/wtf', function *() {
-    this.body = {"wtf": true};
-  });
+  .get('/wtf', new Ctrl().go);
 
 const opts = {
   key: fs.readFileSync(process.cwd() + '/cert/server.key'),
